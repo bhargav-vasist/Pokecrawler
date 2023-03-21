1e52cfcdae1cc6691426079d1240b4937118c841
bhargav-vasist <bhargavclick@gmail.com>
2023-03-21 16:15:24 -0400

    Add empty controller + new group for pokedex page

---
+++ Pokecrawler/Controllers/PokeDetails/PKPokeDetailViewController.swift
@@ -0,0 +1,17 @@
+//
+//  PKPokeDetailViewController.swift
+//  Pokecrawler
+//
+//  Created by Bhargav Vasist on 21/03/23.
+//
+
+import UIKit
+
+class PKPokeDetailViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configureNavigation()
+    }
+    
+    private func configureNavigation()
+}
